2159489f94e3c5ee71e8c84bc3d2425458dd2b3e
shawn <shawn0326@163.com>
2016-04-21 17:43:41 +0800

    modify draw data

--- src/DrawData.js
+++ src/DrawData.js
@@ -15,6 +15,11 @@ var DrawData = function() {
 // draw data object pool
 DrawData.pool = [];
 
+// create some draw data
+for(var i = 0; i < 300; i++) {
+    DrawData.pool.push(new DrawData());
+}
+
 DrawData.getObject = function() {
     return DrawData.pool.length > 0 ? DrawData.pool.pop() : new DrawData();
 };
