0e2bba13cfb7b2d20afd7ecca4361c41e938f1d1
shawn <shawn0326@163.com>
2016-05-13 15:57:18 +0800

    fix state bug

--- build/all.js
+++ build/all.js
@@ -5,8 +5,6 @@
 var State = function() {
     this.startTime = Date.now();
     this.frameCount = 0;
-    this.draw = "";
-    this.fps = "";
 
     this.dom = document.createElement("div");
     this.dom.style.cssText = "background:rgba(0, 0, 0, 0.8);position:absolute;top:0;left:0;padding:10px;min-width:180px;height:80px;fontSize:26px;color:green";
@@ -17,21 +15,17 @@ State.prototype.update = function(draw) {
     if(endTime - this.startTime < 1000) {
         this.frameCount ++;
     } else {
-        this.fps = Math.min(this.frameCount + 1, 60);
+        var fps = Math.min(this.frameCount + 1, 60);
+        this.show(fps, draw || "[not input!]");
+
         this.startTime = endTime;
-        frameCount = 0;
-
-        if(draw) {
-            this.draw = draw;
-        }
-
-        this.show();
+        this.frameCount = 0;
     }
 }
 
-State.prototype.show = function() {
-    this.dom.innerHTML = "FPS :" + this.fps + "</br>"
-                     + "DRAW:" + this.draw;
+State.prototype.show = function(fps, draw) {
+    this.dom.innerHTML = "FPS :" + fps + "</br>"
+                     + "DRAW:" + draw;
 }
 
 State.prototype.getDom = function() {
